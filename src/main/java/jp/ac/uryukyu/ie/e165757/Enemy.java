3dd1e3454e75958e8fd4d9ab01c07baccd42f84e
Shota Nagamine <e165757@ie.u-ryukyu.ac.jp>
2017-01-15 16:47:11 +0900

    Report6

---
+++ src/main/java/jp/ac/uryukyu/ie/e165757/Enemy.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e165757;
+
+/**
+ * Created by e165757 on 2017/01/15.
+ */
+
+public class Enemy extends LivingThing {
+    Enemy(String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
+
+    }
+}
---
+++ src/main/java/jp/ac/uryukyu/ie/e165757/Hero.java
@@ -0,0 +1,10 @@
+package jp.ac.uryukyu.ie.e165757;
+
+/**
+ * Created by e165757 on 2017/01/15.
+ */
+public class Hero extends LivingThing {
+    Hero(String name, int maximumHP, int attack){
+        super(name,maximumHP,attack);
+    }
+}
